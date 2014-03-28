239c916467de24d51489b24cdd4f571a93c2d7a0
Shawn Toffel <shawn.toffel@gmail.com>
2014-03-27 20:48:11 -0600

    Use WPF for GUI

---
+++ LaunchWait/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LaunchWait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            stackPanel.Children.Add(new UserControls.ProgramTimer());
+            stackPanel.Children.Add(new UserControls.ProgramTimer());
+            stackPanel.Children.Add(new UserControls.ProgramTimer());
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void cancelAllButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
